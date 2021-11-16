76cff0e6ab9cd5483fe80ebf61b4689c682a41a9
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-11-16 19:49:44 -0300

    Layout component.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -7,8 +8,11 @@ export default function Home() {
     <div className={`
       flex h-screen justify-center items-center
       bg-gradient-to-r from-purple-500 to to-blue-600
+      text-white
     `}>
-        <span className="text-4xl">Text</span>
+        <Layout title="Simple Register">
+          <span>Content</span>
+        </Layout>
     </div>
   )
 }
