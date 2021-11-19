27b17d593c36dd30bcab54c4569ba00ac1b5bf47
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-11-19 06:16:21 -0300

    Table contents.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,14 +4,39 @@ interface TableProps {
     clients: Client[]
 }
 
-export default function Table(props) {
-    return (
-        <table>
+export default function Table(props: TableProps) {
+
+    function renderHead() {
+        return (
             <tr>
                 <th>ID</th>
                 <th>Name</th>
                 <th>Age</th>
             </tr>
+        )
+    }
+
+    function renderData() {
+        return props.clients?.map((client, i) => {
+            return (
+                <tr key={client.id}>
+                    <td>{client.id}</td>
+                    <td>{client.name}</td>
+                    <td>{client.age}</td>
+                </tr>
+            )
+        })
+    }
+
+    return (
+        <table>
+            <thead>
+                {renderHead()}
+            </thead>
+            <tbody>
+                {renderData()}
+            </tbody>
         </table>
+ 
     )
 }
