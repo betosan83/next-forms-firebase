c8a8193704501948c4e3201874134d68a97e56d9
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-11-29 06:50:08 -0300

    Client Repository with Firebase

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,7 +1,7 @@
 import 'firebase/firestore'
-import firebase from 'firebase/app'
+import firebase from 'firebase'
 
-if (!firebase.getApps.length) {
+if (!firebase.apps.length) {
     firebase.initializeApp({
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
